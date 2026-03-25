c02b9699c09b409f7c44729bc11727f426cd5986
Hanns Peter <hprincivil@Mac-Studio.local>
2026-03-25 07:43:28 -0400

    Update Homebrew formula for v1.0.0

--- packaging/homebrew/sxmc.rb
+++ packaging/homebrew/sxmc.rb
@@ -1,8 +1,8 @@
 class Sxmc < Formula
   desc "Sumac: bring out what your tools can do (Skills x MCP x CLI)"
   homepage "https://github.com/aihxp/sumac"
-  url "https://github.com/aihxp/sumac/archive/refs/tags/v0.2.45.tar.gz"
-  sha256 "ad0590c36c0ef1648e0b13a6d70e0ef7de28a915e61ee59bee09bf1fc484f220"
+  url "https://github.com/aihxp/sumac/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "88ead89b164898cef4005d91fe6209039b755abf65b31def264d9dd5bbf40d22"
   license "MIT"
   head "https://github.com/aihxp/sumac.git", branch: "master"
 
